370f5a3815396ae205c47538e45a43c1cbdf54cb
yosdarwin <rahmiatul.rasyiqah@gmail.com>
2022-06-09 23:14:03 +0700

    Section carousel kegiatan

--- js/script.js
+++ js/script.js
@@ -31,4 +31,31 @@ $(document).ready(function () {
             prevEl: ".swiper-button-prev",
         },
     });
+
+    var swiper = new Swiper(".swiper-kegiatan", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        },
+    });
 });
